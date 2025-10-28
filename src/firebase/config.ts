2c806897241797acd7920cd14da0cd2c5c7de576
Jeremy Delmer <23thavenue1@gmail.com>
2025-10-28 17:32:18 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-9587105821-540bd",
+  "appId": "1:339040917257:web:4f10eacce90f7aab4fe5d8",
+  "apiKey": "AIzaSyBQ-tigzpvson13oGkUW2Iv8RbCGLr-dEs",
+  "authDomain": "studio-9587105821-540bd.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "339040917257"
+};
