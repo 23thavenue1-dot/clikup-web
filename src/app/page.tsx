31dba84c00b1ed6af111ac469426a14eaaf1260c
Jeremy Delmer <23thavenue1@gmail.com>
2025-11-24 18:22:57 +0000

    rajoute aussi leffet 1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -61,15 +61,15 @@ export default function Home() {
           </div>
         </header>
 
-        <div className="transition-all duration-200 ease-out hover:border-primary border border-transparent rounded-lg">
+        <div className="transition-all duration-200 ease-out hover:shadow-xl hover:border-primary border border-transparent rounded-lg">
           <Uploader />
         </div>
 
-        <div className="transition-all duration-200 ease-out hover:border-primary border border-transparent rounded-lg">
+        <div className="transition-all duration-200 ease-out hover:shadow-xl hover:border-primary border border-transparent rounded-lg">
           <ImageList />
         </div>
 
-        <div className="transition-all duration-200 ease-out hover:border-primary border border-transparent rounded-lg">
+        <div className="transition-all duration-200 ease-out hover:shadow-xl hover:border-primary border border-transparent rounded-lg">
           <NotesSection />
         </div>
 
