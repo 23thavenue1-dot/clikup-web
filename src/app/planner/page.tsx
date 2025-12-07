fdce179b913a3857d0a37fdd32ad028bf18b2793
Jeremy Delmer <23thavenue1@gmail.com>
2025-12-07 21:23:50 +0000

    Try fixing this error: `Runtime Error: Error: DialogTrigger is not defin

--- src/app/planner/page.tsx
+++ src/app/planner/page.tsx
@@ -1,3 +1,4 @@
+
 'use client';
 
 import { useUser, useFirestore, useCollection, useMemoFirebase } from '@/firebase';
@@ -28,7 +29,7 @@ import { useToast } from '@/hooks/use-toast';
 import { deleteScheduledPost } from '@/lib/firestore';
 import { withErrorHandling } from '@/lib/async-wrapper';
 import { AlertDialog, AlertDialogAction, AlertDialogCancel, AlertDialogContent, AlertDialogDescription, AlertDialogFooter, AlertDialogHeader, AlertDialogTitle } from "@/components/ui/alert-dialog";
-import { Dialog, DialogContent, DialogHeader, DialogTitle, DialogDescription, DialogFooter } from '@/components/ui/dialog';
+import { Dialog, DialogContent, DialogHeader, DialogTitle, DialogDescription, DialogFooter, DialogTrigger } from '@/components/ui/dialog';
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '@/components/ui/select';
 import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
 import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
