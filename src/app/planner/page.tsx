431a0fc98731d4f056acac68daf6a8aa2eebc5dc
Jeremy Delmer <23thavenue1@gmail.com>
2025-12-07 21:21:30 +0000

    ok passons a l'étape suivante

--- src/app/planner/page.tsx
+++ src/app/planner/page.tsx
@@ -1,4 +1,3 @@
-
 'use client';
 
 import { useUser, useFirestore, useCollection, useMemoFirebase } from '@/firebase';
@@ -29,7 +28,7 @@ import { useToast } from '@/hooks/use-toast';
 import { deleteScheduledPost } from '@/lib/firestore';
 import { withErrorHandling } from '@/lib/async-wrapper';
 import { AlertDialog, AlertDialogAction, AlertDialogCancel, AlertDialogContent, AlertDialogDescription, AlertDialogFooter, AlertDialogHeader, AlertDialogTitle } from "@/components/ui/alert-dialog";
-import { Dialog, DialogContent, DialogHeader, DialogTitle, DialogDescription, DialogTrigger } from '@/components/ui/dialog';
+import { Dialog, DialogContent, DialogHeader, DialogTitle, DialogDescription, DialogFooter } from '@/components/ui/dialog';
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '@/components/ui/select';
 import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
 import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
